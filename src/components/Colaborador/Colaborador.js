fc078bbdc58120f2dfd0faa158c09c3059e2e55f
Jhan24b <jhan24b@gmail.com>
2023-10-26 00:01:03 -0500

    anuncios avance

--- src/components/Colaborador/Colaborador.js
+++ src/components/Colaborador/Colaborador.js
@@ -1,31 +1,34 @@
 // SE TRATA DE LOS USUARIOS
 
 import "./Colaborador.css";
-import MiniUser from "../MiniUser/MiniUser"
-import {AiOutlineUserDelete, AiOutlineHeart} from "react-icons/ai";
-import {FcLike} from "react-icons/fc";
+import MiniUser from "../MiniUser/MiniUser";
+import { AiOutlineUserDelete, AiOutlineHeart } from "react-icons/ai";
+import { FcLike } from "react-icons/fc";
 
-const Colaborador = (props)=>{
-    const {titulo, precio, foto, id, fav, ubicacion, producto, usuario} = props.datos;
-    const {eliminarColaborador, like} = props;
-    return <section className="colaborador">
-        {/* <AiOutlineUserDelete onClick={() => eliminarColaborador(id)} className="eliminar"/>
+const Colaborador = (props) => {
+  const { titulo, precio, foto, id, fav, ubicacion, producto, usuario } =
+    props.datos;
+  const { eliminarColaborador, like } = props;
+  return (
+    <section className="colaborador">
+      {/* <AiOutlineUserDelete onClick={() => eliminarColaborador(id)} className="eliminar"/>
         {fav ? <FcLike onClick={() => like(id)}/> : <AiOutlineHeart onClick={() => like(id)}/>} */}
-        <div className="anuncio" style={{backgroundColor: props.bc}}>
-            <div className="anuncio--titulo">{titulo}</div>
-            <div className="anuncio--contenido">
-                <div className="contenido--detalles">
-                    <h4>{producto}</h4>
-                    <h4>{precio}</h4>
-                    <h4>{ubicacion}</h4>
-                </div>
-                <div className="contenido--foto">
-                <img src={foto} alt="foto de anuncio"></img>
-                </div>
-            </div>
-            <MiniUser user={usuario} title = {titulo}></MiniUser>
+      <div className="anuncio" style={{ backgroundColor: props.bc }}>
+        <div className="anuncio--titulo">{titulo}</div>
+        <div className="anuncio--contenido">
+          <div className="contenido--detalles">
+            <h4>{producto}</h4>
+            <h4>{precio}</h4>
+            <h4>{ubicacion}</h4>
+          </div>
+          <div className="contenido--foto">
+            <img src={foto} alt="foto de anuncio"></img>
+          </div>
         </div>
+        <MiniUser user={usuario} title={titulo}></MiniUser>
+      </div>
     </section>
-}
+  );
+};
 
-export default Colaborador; 
+export default Colaborador;
